56bc7ee2bc4e1b0d080b4c6478b5c8798f420bd8
ayush <billore.ayush@gmail.com>
2021-04-11 01:06:34 +0530

    adds support for activestorage

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, polymorphic: true
+  has_many_attached :posts
 
   scope :system_generated, -> { where(system_generated: true) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,10 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 
   has_many :events, as: :creator
+  has_one_attached :avatar
 
   after_save :create_anniversary_events
+  before_create :set_default_avatar
 
   def generate_jwt
     JWT.encode(
@@ -30,5 +32,9 @@ class User < ApplicationRecord
       start_date: doj, end_date: doj, name: "Happy Work Anniversary #{full_name}",
       color: '#0077B5'
     )
+  end
+
+  def set_default_avatar
+    self.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default-avatar')), filename: 'default-avatar', content_type: 'image/jpeg')
   end
 end
