3eec9527a33a9589a38c49dc81a595c4db575447
Karan Valecha <karan.valecha92@gmail.com>
2021-04-13 19:40:30 +0530

    fix the secret key base

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
         id: id,
         exp: 60.days.from_now.to_i,
       },
-      Rails.application.secrets.secret_key_base
+      Rails.application.secret_key_base
     )
   end
 
