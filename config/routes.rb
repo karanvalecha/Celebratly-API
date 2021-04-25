68ed11693227c9b87d7573b853cddcbf634af781
Karan Valecha <karan.valecha92@gmail.com>
2021-04-25 13:18:54 +0530

    default json format for devise

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
               controllers: {
                sessions: 'sessions',
                registrations: 'registrations'
-              }
+              }, defaults: { format: :json }
 end
