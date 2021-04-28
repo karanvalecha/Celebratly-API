9fdb348d6f34e2ed8ca2a145bf2d1ed004017193
ayush <billore.ayush@gmail.com>
2021-04-28 18:41:41 +0530

    send notification on app and slack on completion of video

--- app/jobs/publish_video_job.rb
+++ app/jobs/publish_video_job.rb
@@ -2,5 +2,8 @@ class PublishVideoJob < ApplicationJob
   def perform(occurrence)
     video_path = OccurrenceVideoCreator.new(occurrence).make_video
     # send notification if any
+
+    SendSlackNotification.new.perform
+    SystemWisherJob.new.perform
   end
 end
