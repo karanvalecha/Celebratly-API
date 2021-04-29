4bac83ada8a7af2e70029467aa094ae5b6091eb2
Karan Valecha <karan.valecha92@gmail.com>
2021-04-29 22:14:57 +0530

    fix text and non recurring orccurrences

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -1,11 +1,22 @@
 class Occurrence < ApplicationRecord
   belongs_to :event
 
-  has_one :reference, through: :event
+  has_one :reference, through: :event, source_type: 'User'
   has_many :status_uploads, dependent: :destroy
 
 
   has_one_attached :published_video, service: :cloudinary_compiled_videos
+
+  # override
+  def title
+    if event.birthday?
+      "#{reference.short_name}'s Birthday!"
+    elsif event.work_anniversary?
+      "#{reference.short_name}'s Workiversary!"
+    else
+      super
+    end
+  end
 
   def slug
     "#{event.name} #{id}".parameterize
--- app/views/profile/show.json.jbuilder
+++ app/views/profile/show.json.jbuilder
@@ -1,5 +1,8 @@
 json.partial! "users/user", user: current_user
 
 json.occurrences do
-  json.array! current_user.occurrences, partial: 'occurrences/occurrence', as: :occurrence
+  json.array!(
+    current_user.occurrences.joins(:published_video_attachment),
+    partial: 'occurrences/occurrence', as: :occurrence
+  )
 end
