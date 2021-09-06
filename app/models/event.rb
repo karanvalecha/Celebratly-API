2896b0be9c6af5a443941e6a7816bff91d3f2384
Karan Valecha <karan.valecha92@gmail.com>
2021-09-06 18:12:09 +0530

    undo temp changes to the model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,3 @@
-
 class Event < ApplicationRecord
   include ActionView::Helpers::TextHelper
 
@@ -78,12 +77,11 @@ class Event < ApplicationRecord
     return unless system_generated?
     return unless next_occurrence_this_year
 
-    occurrences.create({
+    occurrences.create(
       title: name,
       start_at: next_occurrence_this_year.beginning_of_day,
       end_at: next_occurrence_this_year.end_of_day,
       caption: get_default_caption
-    }
     )
   end
 
